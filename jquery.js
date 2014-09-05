83adf6dfdd204cfc15a0508d588cf264be442d97
VasiliyRodin <rodinvasiliy@gmail.com>
2014-09-04 21:14:26 -0700

    Added responsive design and 3 columns

--- jquery.js
+++ jquery.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+/*
 $(document).ready(function(){
     $("p").hide();
     
@@ -25,4 +25,10 @@ $(document).ready(function(){
 	$("#fadeButton").click(function() {
 		$("#fadeHeader").fadeToggle(500);
 	});
+});
+
+*/
+
+$(document).ready(function(){    
+
 });
